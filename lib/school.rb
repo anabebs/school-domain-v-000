4695fd103d909d8a6c47c0097ced67737673a127
Ana Bebic <anabebic.x@gmail.com>
2018-07-07 06:26:05 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+  # code here!
+class School
+  attr_reader :name, :roster
+  
+def initialize(name)
+  @name= name
+  @roster= {}
+end
+
+def add_student(name, grade)
+  self.roster[grade] ||=[]
+  self.roster[grade] << name
+  #if @roster.has_key?(grade)
+   # @roster[grade] << name
+  #else
+    #@roster[grade] = [name]
+end
+
+
+
+end
